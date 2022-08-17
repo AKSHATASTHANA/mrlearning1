a446b1ce6a5d9191df2240edc323327dddf75fe4
Shiwam03 <d.shiwam03@gmail.com>
2022-08-18 03:53:30 +0530

    connections

--- mrweb/urls.py
+++ mrweb/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('home/', views.home, name="home"),
     path('about/', views.about, name="about"),
     path('courses/', views.courses, name="courses"),
     path('contact/', views.contact, name="contact"),
